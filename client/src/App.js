de6c822128151162006a75d55f6581a2b991f4e8
Maxim Schuckin <shuckin@mail.ru>
2021-07-30 00:46:57 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import "tailwindcss/tailwind.css"
+
+
+
+function App() {
+  return (
+    <div className="App ">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div className="high-specificity">
+        <div className="backdrop-filter backdrop-grayscale md:backdrop-grayscale-0 ...">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore debitis voluptas accusantium dolores itaque minima, harum quasi error animi. Recusandae assumenda aliquid sapiente! Corporis voluptate a alias dolor hic magnam cupiditate, ullam eius, ipsum quae ipsa doloremque! Aliquid, architecto totam? Ad corporis possimus natus, obcaecati dolore, aspernatur voluptate maiores vel porro, perspiciatis dolor assumenda nisi commodi cum veniam minima ea doloremque! Voluptate quas asperiores vero eius id corrupti similique praesentium tenetur corporis, iusto quod voluptas voluptatem magnam consectetur sit qui explicabo deserunt temporibus earum commodi, dignissimos laboriosam totam nemo. Est, saepe? Hic aut suscipit odit nam aliquid dignissimos voluptate quibusdam.
+        </div>
+      </div>
+    </div>
+  )
+}
